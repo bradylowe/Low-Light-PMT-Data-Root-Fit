255653d47e9e2ca729529b2322684c69f3afa926
Brady Lowe <lowebra2@isu.edu>
2018-10-13 21:53:44 -0600

    added dataAnalyzerTest.c for running root functions from dataAnalyzer.c

---
+++ run_root.c
@@ -0,0 +1,30 @@
+// Brady Lowe // lowebra2@isu.edu
+
+#include <TFile.h>
+#include <TDirectory.h>
+#include <TTree.h>
+#include <TROOT.h>
+#include <TMath.h>
+#include <TChain.h>
+#include <TH1F.h>
+#include <TF1.h>
+
+#include "Math/SpecFunc.h"
+#include "dataAnalyzer.c"
+
+#include <TMinuit.h>
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TStyle.h>
+#include <TAxis.h>
+#include <TLine.h>
+
+
+int run_root(string histogram){
+
+	createPngFromNumbers(histogram);
+
+	return 1;
+}
+
+
