72280a2d8d037e452bf6cc72fbf766ebbab31c8d
General DAQ User <daq@daq3.physics.isu.edu>
2018-11-01 14:33:08 -0600

    added more runs, made sql_get_average work a little better, added setup.txt file

--- sql_get_average.sh
+++ sql_get_average.sh
@@ -1,32 +1,22 @@
 
-pmt=1
-
-# Define default conditions
-defaults="gain > 0 AND gain_error > 0 AND gain_percent_error < 10 AND con_gain > 10 AND con_ll < 100 AND con_ll > 0 AND mu_out < 10"
-
 # Look at input parameters
-if [ $# -eq 0 ] ; then
+if [ $# -ne 3 ] ; then
 	echo error
 	exit
-elif [ $# -eq 1 ] ; then
-	fit_cond=${defaults}
-elif [ $# -eq 2 ] ; then
-	fit_cond="$2 AND ${defaults}"
-else
-	run_cond=$2
-	fit_cond="$3 AND ${defaults}"
 fi
 
+run_cond=$2
+fit_cond=$3
 # Grab parameter to find average of
 table=$(echo $1 | awk -F'.' '{print $1}')
 param=$(echo $1 | awk -F'.' '{print $2}')
 
 # Query database using info from run_params and fit_results tables
-if [ ${#run_cond} -gt 0 ] ; then
-	query="USE gaindb; SELECT ${param} FROM ${table} WHERE fit_results.run_id IN (SELECT run_id FROM run_params WHERE ${run_cond} AND pmt=${pmt}) AND ${fit_cond};"
+if [[ ${table} == "fit_results" ]] ; then
+	query="USE gaindb; SELECT ${param} FROM fit_results WHERE fit_results.run_id IN (SELECT run_id FROM run_params WHERE ${run_cond}) AND ${fit_cond};"
 	values=$(mysql --defaults-extra-file=~/.mysql.cnf -Bse "${query}")
 else
-	query="USE gaindb; SELECT ${param} FROM ${table} WHERE ${fit_cond};"
+	query="USE gaindb; SELECT ${param} FROM run_params WHERE run_params.run_id IN (SELECT run_id FROM fit_results WHERE ${fit_cond}) AND ${run_cond};"
 	values=$(mysql --defaults-extra-file=~/.mysql.cnf -Bse "${query}")
 fi
 
@@ -40,6 +30,10 @@ for item in ${values} ; do
 	sum=$((sum + item))
 	count=$((count + 1))
 done
+if [ ${count} -eq 0 ] ; then
+	echo no runs
+	exit
+fi
 ave=$((sum / count))
 
 sum=0
