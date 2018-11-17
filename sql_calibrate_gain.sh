96898f752c0c85bbedcd21fd75501dca20ef681d
Brady Lowe <lowebra2@isu.edu>
2018-11-16 19:13:50 -0700

    fixed error in sql_measure_ll and sql_calibrate_ll, calibrated pmt1 ll

--- sql_calibrate_gain.sh
+++ sql_calibrate_gain.sh
@@ -24,7 +24,7 @@ for pmt in ${pmt_list} ; do
 	if [ ${pmt} -le 4 ] ; then
 		hv_list="2000 1975 1950 1925 1900 1800 1700 1600"
 	else
-		hv_list="1350 1300 1250 1200 1150 1100 1050 100"
+		hv_list="1350 1300 1250 1200 1150 1100 1050 1000"
 	fi
 	if [ ${#hv} -gt 0 ] ; then
 		hv_list=${hv}
@@ -38,10 +38,16 @@ for pmt in ${pmt_list} ; do
 		new_val=${new_val%,*}
 		check=$(echo ${new_val} | grep .)
 		if [[ ${check} != "no fits" && ${check:0:1} != "-" ]] ; then
-			old_line=$(grep ${hv}, calibration/pmt${pmt}_gain.csv)
+			# Check for existing value, store value if none exists
+			old_line=$(grep ${hv}, ${csv_file})
 			old_val=$(echo ${old_line} | awk -F',' '{print $2}')
+			if [ ${#old_line} -eq 0 ] ; then
+				echo ${hv},${new_val} >> ${csv_file}
+			fi
+			# Create new line from old line using new value
 			new_line=$(echo ${old_line} | sed "s/${old_val}/${new_val}/g")
-			read -p "Update pmt${pmt} hv${hv} with ${new_val}?  " choice
+			# Check with the user before changing the file
+			read -p "Update pmt${pmt} hv${hv} with ${out}?  " choice
 			if [[ ${choice:0:1} == "y" || ${choice:0:1} == "Y" ]] ; then
 				sed -i "s/${old_line}/${new_line}/g" ${csv_file}
 			fi
--- sql_calibrate_ll.sh
+++ sql_calibrate_ll.sh
@@ -1,7 +1,7 @@
 
 # Initialize input parameters
 pmt_list=""
-ll_list=""
+ll_list="20 25 30 35 40 45 50 55 60 70 80 90 100"
 
 # Parse input
 for item in $* ; do
@@ -23,16 +23,21 @@ for pmt in ${pmt_list} ; do
 	
 	# Loop through all ll's in ll list
 	for ll in ${ll_list} ; do
-		./sql_select_ids.sh id="fit_id" recent=1 good=1 ll=${ll} pmt=${pmt} >> /dev/null
-		out=$(./sql_average.sh sig_out)
+		./sql_select_ids.sh id="fit_id" run_cond="filter=7" recent=1 good=1 ll=${ll} pmt=${pmt} >> /dev/null
+		out=$(./sql_average.sh mu_out)
 		new_val=${out#*:  (}
 		new_val=${new_val%,*}
 		check=$(echo ${new_val} | grep .)
 		if [[ ${check} != "no fits" && ${check:0:1} != "-" ]] ; then
+			# Check for existing value, store value if none exists
 			old_line=$(grep ${ll}, ${csv_file})
 			old_val=$(echo ${old_line} | awk -F',' '{print $2}')
+			if [ ${#old_line} -eq 0 ] ; then
+                                echo ${hv},${new_val} >> ${csv_file}
+                        fi
+                        # Create new line from old line using new value
 			new_line=$(echo ${old_line} | sed "s/${old_val}/${new_val}/g")
-			read -p "Update pmt${pmt} ll${ll} with ${new_val}?  " choice
+			read -p "Update pmt${pmt} ll${ll} with ${out}?  " choice
                         if [[ ${choice:0:1} == "y" || ${choice:0:1} == "Y" ]] ; then
                                sed -i "s/${old_line}/${new_line}/g" ${csv_file}
                         fi
